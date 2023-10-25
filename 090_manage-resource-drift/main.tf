88d2defe01b0ea188e0748a6f229c4da4ec82403
CloudCoder101 <robert.zebrowski.devops@gmail.com>
2023-10-25 18:38:05 -0500

    add correct subnet

--- 090_manage-resource-drift/main.tf
+++ 090_manage-resource-drift/main.tf
@@ -12,8 +12,9 @@ provider "aws" {
   region  = "us-east-1"
 }
 resource "aws_instance" "my_server" {
-  ami           = "ami-087c17d1fe0178315"
+  ami           = "ami-0df435f331839b2d6"
   instance_type = "t2.micro"
+  subnet_id     = "subnet-0a96779c00837e82f"  # Specify your subnet ID here
 	tags = {
 		Name = "MyServer"
 	}
