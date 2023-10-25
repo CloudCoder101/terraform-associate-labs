44b77df43d7592f2e123eb0507ca37b4e335fc02
CloudCoder101 <robert.zebrowski.devops@gmail.com>
2023-10-25 17:52:02 -0500

    not sure what this change is

--- 070_terraform_state/main.tf
+++ 070_terraform_state/main.tf
@@ -15,8 +15,8 @@ provider "aws" {
 resource "aws_instance" "our_server" {
   ami           = "ami-0df435f331839b2d6"
   instance_type = "t2.micro"
-  
-  subnet_id     = "subnet-0a96779c00837e82f"  # Specify your subnet ID here
+
+  subnet_id = "subnet-0a96779c00837e82f" # Specify your subnet ID here
   # vpc_id = "vpc-0cf271fafe4938239"  # Specify your VPC ID here
 
   tags = {
