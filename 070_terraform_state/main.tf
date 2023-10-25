8a7aac63a070c442be2ecf6779a498e312cc9c54
Robert Zebrowski <58918370+CloudCoder101@users.noreply.github.com>
2023-10-25 10:17:30 -0500

    070 Get ec2 working to aws via terraform (#1)
    Co-authored-by: CloudCoder101 <robert.zebrowski.devops@gmail.com>

--- 070_terraform_state/main.tf
+++ 070_terraform_state/main.tf
@@ -11,12 +11,17 @@ provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
+
 resource "aws_instance" "our_server" {
-  ami           = "ami-087c17d1fe0178315"
+  ami           = "ami-0df435f331839b2d6"
   instance_type = "t2.micro"
-	tags = {
-		Name = "MyServer"
-	}
+  
+  subnet_id     = "subnet-0a96779c00837e82f"  # Specify your subnet ID here
+  # vpc_id = "vpc-0cf271fafe4938239"  # Specify your VPC ID here
+
+  tags = {
+    Name = "MyServer"
+  }
 }
 
 output "public_ip" {
